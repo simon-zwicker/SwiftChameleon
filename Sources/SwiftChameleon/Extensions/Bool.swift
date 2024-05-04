c702e7e43382121a428a3b90b1e9540a5460d69a
Nico <fohlen_autos_0e@icloud.com>
2024-05-05 00:10:28 +0200

    Bool Extension

---
+++ Sources/SwiftChameleon/Extensions/Bool.swift
@@ -0,0 +1,18 @@
+//  File.swift
+//  
+//
+//  Created by Nico on 05.05.24.
+//  
+//
+
+import Foundation
+
+public extension Bool {
+    mutating func setTrue() {
+        self = true
+    }
+    
+    mutating func setFalse() {
+        self = false
+    }
+}
