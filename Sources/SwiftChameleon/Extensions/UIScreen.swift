60fe5e98a69ec815828bd939065739bf15a11647
Nico <fohlen_autos_0e@icloud.com>
2024-05-16 20:14:54 +0200

    UI Screen Extension

---
+++ Sources/SwiftChameleon/Extensions/UIScreen.swift
@@ -0,0 +1,15 @@
+//  File.swift
+//  
+//
+//  Created by Nico on 16.05.24.
+//  
+//
+
+import Foundation
+import SwiftUI
+
+public extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
