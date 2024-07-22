303d06041a038c5d1be4d268ab23a9b0bde95795
MiaKoring <143010553+MiaKoring@users.noreply.github.com>
2024-07-22 05:01:45 -0700

    Fixed macos incompatibility

--- Sources/SwiftChameleon/Extensions/Color.swift
+++ Sources/SwiftChameleon/Extensions/Color.swift
@@ -11,18 +11,18 @@ public extension Color {
     var components: (red: CGFloat, green: CGFloat, blue: CGFloat, opacity: CGFloat) {
         #if canImport(UIKit)
         typealias NativeColor = UIColor
-        #elseif canImport(AppKit)
-        typealias NativeColor = NSColor
         #endif
 
         var r: CGFloat = 0
         var g: CGFloat = 0
         var b: CGFloat = 0
         var o: CGFloat = 0
-
+        
+        #if canImport(UIKit)
         guard NativeColor(self).getRed(&r, green: &g, blue: &b, alpha: &o) else {
             return (0, 0, 0, 0)
         }
+        #endif
         return (r, g, b, o)
     }
 
