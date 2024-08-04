a4cd01c7eb50e5f4ff850aa1ade3f914197cf5de
Simon Zwicker <github@zwicker.io>
2024-08-04 23:04:56 +0200

    Color -> Hex, Hex -> Color

--- Sources/SwiftChameleon/Extensions/Color.swift
+++ Sources/SwiftChameleon/Extensions/Color.swift
@@ -9,35 +9,92 @@ import SwiftUI
 
 public extension Color {
     var components: (red: CGFloat, green: CGFloat, blue: CGFloat, opacity: CGFloat) {
-        #if canImport(UIKit)
+#if canImport(UIKit)
         typealias NativeColor = UIColor
-        #endif
-
+#endif
+        
         var r: CGFloat = 0
         var g: CGFloat = 0
         var b: CGFloat = 0
         var o: CGFloat = 0
         
-        #if canImport(UIKit)
+#if canImport(UIKit)
         guard NativeColor(self).getRed(&r, green: &g, blue: &b, alpha: &o) else {
             return (0, 0, 0, 0)
         }
-        #endif
+#endif
         return (r, g, b, o)
     }
-
+    
     func lighter(by percentage: CGFloat = 30.0) -> Color {
         return self.adjust(by: abs(percentage) )
     }
-
+    
     func darker(by percentage: CGFloat = 30.0) -> Color {
         return self.adjust(by: -1 * abs(percentage) )
     }
-
+    
     func adjust(by percentage: CGFloat = 30.0) -> Color {
         return Color(red: min(Double(self.components.red + percentage/100), 1.0),
                      green: min(Double(self.components.green + percentage/100), 1.0),
                      blue: min(Double(self.components.blue + percentage/100), 1.0),
                      opacity: Double(self.components.opacity))
     }
+    
+    init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+        
+        var rgb: UInt64 = 0
+        
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 1.0
+        
+        let length = hexSanitized.count
+        
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
+        
+        if length == 6 {
+            r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+            g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+            b = CGFloat(rgb & 0x0000FF) / 255.0
+            
+        } else if length == 8 {
+            r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
+            g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
+            b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
+            a = CGFloat(rgb & 0x000000FF) / 255.0
+            
+        } else {
+            return nil
+        }
+        
+        self.init(red: r, green: g, blue: b, opacity: a)
+    }
+    
+#if canImport(UIKit)
+    func toHex() -> String {
+        let uic = UIColor(self)
+        guard let components = uic.cgColor.components, components.count >= 3 else {
+            return nil
+        }
+        
+        let r = Float(components[0])
+        let g = Float(components[1])
+        let b = Float(components[2])
+        var a = Float(1.0)
+        
+        if components.count >= 4 {
+            a = Float(components[3])
+        }
+        
+        if a != Float(1.0) {
+            return String(format: "%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
+        } else {
+            return String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+        }
+    }
+#endif
 }
